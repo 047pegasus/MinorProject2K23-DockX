b83ed234525cbc6cfe7be37cf30e964b4c6b2abb
Damncode12 <damncode12@gmail.com>
2023-05-06 20:44:00 +0530

    add:base-main.py ⚡

---
+++ main.py
@@ -0,0 +1,20 @@
+from customtkinter import *
+
+set_appearance_mode("dark")  # Modes: system (default), light, dark
+set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+root=CTk()
+root.geometry("500x300")
+  
+frame = CTkFrame(master=root, width=200, height=200)
+canvas_default = CTkCanvas(frame,bg = "Grey",height = "200",width = 200)
+
+
+canvas_default.pack()
+  
+frame.pack()  
+
+
+
+
+root.mainloop()
