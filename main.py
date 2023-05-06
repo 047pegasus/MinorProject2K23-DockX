13e4595c9fb3932b0f0bc17b78620ecbae8e99b3
Damncode12 <damncode12@gmail.com>
2023-05-06 20:58:30 +0530

    mod:main.py ✅

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ set_appearance_mode("dark")  # Modes: system (default), light, dark
 set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 root=CTk()
-root.geometry("500x300")
+root.geometry("1366x720")
   
 frame = CTkFrame(master=root, width=200, height=200)
 canvas_default = CTkCanvas(frame,bg = "Grey",height = "200",width = 200)
@@ -13,8 +13,5 @@ canvas_default = CTkCanvas(frame,bg = "Grey",height = "200",width = 200)
 canvas_default.pack()
   
 frame.pack()  
-
-
-
 
 root.mainloop()
