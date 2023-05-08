c4d929375650488b53dfa7963a1c30def14862d6
047pegasus <tanishqoct11@gmail.com>
2023-05-08 15:14:34 +0530

    add:owl_image

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PIL import ImageTk, Image
 from tkinter import PhotoImage
 from customtkinter import *
 
@@ -9,15 +10,16 @@ root.geometry("1920x1080")
 frame = CTkFrame(master=root)
 canvas_default = CTkCanvas(frame,bg = "Grey",height = "1920",width = 300)
 
-#img = PhotoImage(file=".\owl.jpg")
-#ctk.CTkButton(root, image = img).pack(side = LEFT)
-
-canvas_default.grid(row=0,column=0,padx=20)
-button = CTkButton(master=root,text="Logout",fg_color='midnight blue', width=120,height=25,corner_radius=20).grid(row=0,column=0,padx=20,pady=(0,200)
-
-canvas_default = CTkCanvas(root,bg = "Green",height = 1080,width = 300)
+img = Image.open("owl.jpg")
+picimg=img.resize((100,100))
+tkimg = ImageTk.PhotoImage(picimg)
+CTkButton(canvas_default, image = tkimg).grid(row=0,column=0)
                                                                                                                   
 label = CTkLabel(master=canvas_default,text="Ayushi",fg_color='black', width=120,height=25).grid(row=0,column=1)
+
+button = CTkButton(master=root,text="Logout",fg_color='midnight blue', width=120,height=25,corner_radius=20).grid(row=0,column=0,padx=20,pady=(0,200))
+
+canvas_default.grid(row=0,column=0,padx=20)
 
 root.mainloop()
 
