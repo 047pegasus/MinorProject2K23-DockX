c45db5b176605fbf161b43842f65389d90de3a52
Damncode12 <damncode12@gmail.com>
2023-05-09 08:35:15 +0530

    add: logout button

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 root=CTk()
 root.geometry("1920x1080")
 frame = CTkFrame(master=root)
-canvas_default = CTkCanvas(frame,bg = "Grey",height = "1920",width = 500)
+canvas_default = CTkCanvas(frame,bg = "Grey",height = "1920",width = 300)
 
 #img = PhotoImage(file=".\owl.jpg")
 #ctk.CTkButton(root, image = img).pack(side = LEFT)
 
-canvas_default.pack()
-label = CTkLabel(master=root,text="Ayushi",fg_color='black', width=120,height=25).grid(row=0,column=1)
+canvas_default.grid(row=0,column=0,padx=20)
+button = CTkButton(master=root,text="Logout",fg_color='midnight blue', width=120,height=25,corner_radius=20).grid(row=0,column=0,padx=20,pady=(0,200))
 
 
 frame.grid(row=0,column=0,padx=20)
