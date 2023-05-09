cf29057a3bf11a16f9ceea714e6189f7f0da222c
Tanishq Agarwal <68327491+047pegasus@users.noreply.github.com>
2023-05-10 01:04:37 +0530

    Update main.py: add arcs to main.py

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from PIL import Image, ImageTk
 set_appearance_mode("dark")  # Modes: system (default), light, dark
 set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-
 def fun_yes():
-    print("p")
+    messagebox.showinfo('Return','All containers are closed.')
+    y=messagebox.askyesno("Confirmation","Do you want to Logout?")
+    if y==True:
+        root.destroy()
+    else:
+        messagebox.showinfo('Return','You will now return to the main screen.')
 
 def fun_no():
     y=messagebox.askyesno("Confirmation","Do you want to Logout?")
@@ -23,6 +27,16 @@ def fun():
     else:
         fun_no()
 
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+CTkCanvas.create_circle = _create_circle
+
+def _create_circle_arc(self, x, y, r, **kwargs):
+    if "start" in kwargs and "end" in kwargs:
+        kwargs["extent"] = kwargs["end"] - kwargs["start"]
+        del kwargs["end"]
+    return self.create_arc(x-r, y-r, x+r, y+r, **kwargs)
+CTkCanvas.create_circle_arc = _create_circle_arc
 
 root=CTk()
 screen_width =(int)(root.winfo_screenwidth())
@@ -44,21 +58,36 @@ containerlabel = CTkButton(master=framemenu,text="Containers",font=("Roboto", 20
 cpustatslabel = CTkButton(master=framemenu,text="CPU Statistics",font=("Roboto", 20), cursor='arrow',fg_color='gray19',hover_color='gray11',text_color='White').pack(side=TOP,padx=0,pady=(20,10))
 
 
-button = CTkButton(master=framemenu,text="Logout",font=("Roboto", 20), fg_color='midnight blue', width=120,height=30,corner_radius=20,command=fun).pack(side=BOTTOM,padx=0,pady=(200,100))
+button = CTkButton(master=framemenu,text="Logout",font=("Roboto", 20), fg_color='midnight blue', width=120,height=30,corner_radius=10,command=fun).pack(side=BOTTOM,padx=0,pady=(200,100))
 framemenu.pack(side=LEFT,fill=BOTH,padx=0,pady=0)
 
 #mainWindowFrame                                                                                                                                                                                                                                    
 frame_main = CTkFrame(master=root, width=1000, height=800, fg_color="Black")
 
 can_def1 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
-can_def1.pack(side=RIGHT, expand=True, padx=20)
+
+can_def1.create_circle(200, 200, 100, fill="gold2", outline="", width=4)
+can_def1.create_circle_arc(200, 200, 100, fill="grey", outline="", start=45, end=140)
+can_def1.create_circle(200, 200, 70, fill="MediumPurple4", outline="", width=4)
+
+can_def1.pack(side=RIGHT, expand=True, padx=(20,0))
 
 can_def2 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
-can_def2.pack(side=RIGHT, expand=True, padx=20)
+
+can_def2.create_circle(200, 200, 100, fill="spring green", outline="", width=4)
+can_def2.create_circle_arc(200, 200, 100, fill="grey", outline="", start=165, end=220)
+can_def2.create_circle(200, 200, 70, fill="MediumPurple4", outline="", width=4)
+
+can_def2.pack(side=RIGHT, expand=True, padx=(20,0))
 
 can_def3 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
-can_def3.pack(side=RIGHT, expand = True,padx=20)
 
-frame_main.pack(side=RIGHT,padx=20,expand=True)
+can_def3.create_circle(200, 200, 100, fill="coral", outline="", width=4)
+can_def3.create_circle_arc(200, 200, 100, fill="grey", outline="", start=165, end=300)
+can_def3.create_circle(200, 200, 70, fill="MediumPurple4", outline="", width=4)
+
+can_def3.pack(side=RIGHT, expand = True,padx=(20,20))
+
+frame_main.pack(side=RIGHT,padx=0,expand=True,fill=BOTH)
 
 root.mainloop()
