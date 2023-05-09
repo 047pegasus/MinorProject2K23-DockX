c699f5d2ad6d9cc04cedb4a83b82819da0a4fb07
Tanishq Agarwal <68327491+047pegasus@users.noreply.github.com>
2023-05-10 00:44:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
-from PIL import ImageTk, Image
-from tkinter import PhotoImage
+from tkinter import PhotoImage,messagebox
 from customtkinter import *
+from PIL import Image, ImageTk
 
 set_appearance_mode("dark")  # Modes: system (default), light, dark
 set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
+
+def fun_yes():
+    print("p")
+
+def fun_no():
+    y=messagebox.askyesno("Confirmation","Do you want to Logout?")
+    if y==True:
+        root.destroy()
+    else:
+        messagebox.showinfo('Return','You will now return to the main screen.')
+
+def fun():
+    x= messagebox.askyesno("Confirmation","Do you want to close all containers?")
+    if x==True:
+        fun_yes()
+    else:
+        fun_no()
+
+
 root=CTk()
-root.geometry("1920x1080")
-frame = CTkFrame(master=root)
-canvas_default = CTkCanvas(frame,bg = "Grey",height = "1920",width = 300)
+screen_width =(int)(root.winfo_screenwidth())
+screen_height =(int)(root.winfo_screenheight())
+root.geometry("%d,%d" % (screen_width,screen_height))
 
-img = Image.open("owl.jpg")
-picimg=img.resize((100,100))
-tkimg = ImageTk.PhotoImage(picimg)
-CTkButton(canvas_default, image = tkimg).grid(row=0,column=0)                                                                                                                  
-label = CTkLabel(master=canvas_default,text="Ayushi",fg_color='black', width=120,height=25).grid(row=0,column=1)
-button = CTkButton(master=root,text="Logout",fg_color='midnight blue', width=120,height=25,corner_radius=20).grid(row=0,column=0,padx=20,pady=(0,200))
-canvas_default.grid(row=0,column=0,padx=20)
-                                                                                                                  
-frame1 = CTkFrame(master=root, width=1500, height=1000)
-canvas_default = CTkCanvas(frame1,bg = "Black",height = "1045",width = 1610)
-canvas_default.grid(row=0,column=0)
-frame1.grid(row = 0, column = 0, padx = (0,800),pady=(0,50))         
+#sidePaneMenu
+framemenu = CTkFrame(master=root,height=screen_height,width=screen_width/4,fg_color="gray19")
 
-frame2 = CTkFrame(master=root, width=400, height=530)
-can_def1 = CTkCanvas(frame2,bg = "Teal",height = "530",width = 400)
-can_def1.grid(row=0,column=0)
-frame2.grid(row=0,column = 0,padx=400,pady=(50,410),sticky="nsw")        
+img = CTkImage(light_image=Image.open("owl.jpg"),dark_image=Image.open("owl.jpg"),size=(150,150))
+imglabel= CTkLabel(framemenu, text='', image = img, corner_radius=50).pack(side=TOP,padx=(10,10),pady=(20,10))
 
-frame3 = CTkFrame(master=root, width=400, height=530)
-can_def2 = CTkCanvas(frame3,bg = "Teal",height = "530",width = 400)
-can_def2.grid(row=0,column=0)
-frame3.grid(row=0,column = 0,padx=800,pady=(50,410),sticky="nsw")
+label = CTkLabel(master=framemenu,text="047pegasus",font=("Roboto", 20), fg_color='gray19',text_color='White').pack(side=TOP,padx=0,pady=(30,10))
 
-frame4 = CTkFrame(master=root, width=400, height=530)
-can_def3 = CTkCanvas(frame4,bg = "Teal",height = "530",width = 400)
-can_def3.grid(row=0,column=0)
-frame4.grid(row=0,column = 0,padx=1200,pady=(50,410),sticky="nsw")
+homelabel = CTkButton(master=framemenu,text="Home",font=("Roboto" ,20), cursor='arrow', fg_color='gray19',hover_color='gray11',text_color='White').pack(side=TOP,padx=0,pady=(150,10))
 
-button = CTkButton(master=root,text="Logout",fg_color='midnight blue', width=120,height=25,corner_radius=20).grid(row=0,column=0,padx=20,pady=(0,200))
+containerlabel = CTkButton(master=framemenu,text="Containers",font=("Roboto", 20), cursor='arrow',fg_color='gray19',hover_color='gray11',text_color='White').pack(side=TOP,padx=0,pady=(20,10))
 
-canvas_default.grid(row=0,column=0,padx=20)
+cpustatslabel = CTkButton(master=framemenu,text="CPU Statistics",font=("Roboto", 20), cursor='arrow',fg_color='gray19',hover_color='gray11',text_color='White').pack(side=TOP,padx=0,pady=(20,10))
+
+
+button = CTkButton(master=framemenu,text="Logout",font=("Roboto", 20), fg_color='midnight blue', width=120,height=30,corner_radius=20,command=fun).pack(side=BOTTOM,padx=0,pady=(200,100))
+framemenu.pack(side=LEFT,fill=BOTH,padx=0,pady=0)
+
+#mainWindowFrame                                                                                                                                                                                                                                    
+frame_main = CTkFrame(master=root, width=1000, height=800, fg_color="Black")
+
+can_def1 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
+can_def1.pack(side=RIGHT, expand=True, padx=20)
+
+can_def2 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
+can_def2.pack(side=RIGHT, expand=True, padx=20)
+
+can_def3 = CTkCanvas(frame_main,bg = "MediumPurple4",height = "530",width = 400)
+can_def3.pack(side=RIGHT, expand = True,padx=20)
+
+frame_main.pack(side=RIGHT,padx=20,expand=True)
 
 root.mainloop()
-
-
-
